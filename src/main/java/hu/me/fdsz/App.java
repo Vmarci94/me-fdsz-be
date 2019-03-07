38da1a530d970556f074ae815d702cc7c9536e8d
varga.marton <varga@jegymester.hu>
2019-03-07 10:00:19 +0100

    fixing package hierarchy

--- src/main/java/hu/me/fdsz/App.java
+++ src/main/java/hu/me/fdsz/App.java
@@ -1,11 +1,9 @@
-package hu.me.fdsz.WebApp;
+package hu.me.fdsz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
 public class App
 {
     public static void main( String[] args )
