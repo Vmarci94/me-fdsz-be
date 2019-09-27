0d927e0c634627ab364ce5d204a2158129795eff
Vmarci94 <vmarci94@gmail.com>
2019-09-27 17:01:00 +0200

    fixit

--- src/main/java/hu/me/fdsz/App.java
+++ src/main/java/hu/me/fdsz/App.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@Profile("dev")
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
