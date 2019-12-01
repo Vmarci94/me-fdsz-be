4a00e1bc6b5b4ae853ffb84ad4861fa1247326d6
Vmarci94 <vmarci94@gmail.com>
2019-12-01 19:15:33 +0100

    hotfix

--- src/main/java/hu/me/fdsz/service/impl/TurnusServiceImpl.java
+++ src/main/java/hu/me/fdsz/service/impl/TurnusServiceImpl.java
@@ -71,7 +71,7 @@ public class TurnusServiceImpl implements TurnusService {
         try {
             Optional<Turnus> turnusO = turnusRepository.findById(turnusId);
             turnusO.ifPresent(turnus -> {
-                if (!getAviableRoomsToTurnus(turnus).isEmpty()) {
+                if (getAviableRoomsToTurnus(turnus).isEmpty()) {
                     turnusRepository.delete(turnus);
                 } else {
                     throw new IllegalArgumentException("Nem lehet olyan turnust törölni, ahol vannak foglalások");
